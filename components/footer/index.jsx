7dbc1d399b6793c4a430f7db76c493b133ceaeee
Giuliano Mozzillo <mozzillation@me.com>
2020-11-28 14:16:33 +0100

    lint fix

---
+++ components/footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.sass'
+
+export const Footer = () => {
+    return (
+        <div className={styles.Footer}>
+            <div className={styles.Wrapper}>ciao</div>
+        </div>
+    )
+}
--- pages/layout.jsx
+++ pages/layout.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types'
 import { Header } from '@component/header'
+import { Footer } from '@component/footer'
 
 export default function Layout({ children }) {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <Footer />
         </>
     )
 }
