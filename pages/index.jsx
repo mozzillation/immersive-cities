c8b8c83fa2e8a92d92c79fd9686bc3168e8678c0
Giuliano Mozzillo <mozzillation@me.com>
2020-12-01 19:22:55 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-
+import Grid from '@component/grid'
 export default function Home() {
     return (
         <>
             <Head>
                 <title>Immersive Cities</title>
+                <Grid />
             </Head>
         </>
     )
