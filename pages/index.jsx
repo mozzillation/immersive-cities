c122bfcbf3fece970448e951339933cca25e6fad
Giuliano Mozzillo <mozzillation@me.com>
2020-12-03 09:20:36 +0100

    mobile homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,22 +2,23 @@ import Head from 'next/head'
 import List from '@component/list'
 import PropTypes from 'prop-types'
 import { getAllCities } from '@api'
+import { useDeviceDetect } from '@utils'
 
 export default function Home({ cities }) {
+    const { isMobile } = useDeviceDetect()
+
     return (
-        <>
+        <div>
             <Head>
                 <title>Immersive Cities</title>
             </Head>
 
-            <List cities={cities} />
-        </>
+            {isMobile ? <List cities={cities} /> : null}
+        </div>
     )
 }
 
 export async function getStaticProps() {
-    console.log('static props', await getAllCities())
-
     return {
         props: {
             cities: await getAllCities()
--- utils/index.jsx
+++ utils/index.jsx
@@ -1,5 +1,24 @@
+import React from 'react'
+
 export function getKeyByValue(object, value) {
     if (Object.getOwnPropertyNames(object).find((key) => key === value)) {
         return object[value]
     }
+}
+
+export function useDeviceDetect() {
+    const [isMobile, setMobile] = React.useState(false)
+
+    React.useEffect(() => {
+        const userAgent =
+            typeof window.navigator === 'undefined' ? '' : navigator.userAgent
+        const mobile = Boolean(
+            userAgent.match(
+                /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+            )
+        )
+        setMobile(mobile)
+    }, [])
+
+    return { isMobile }
 }
